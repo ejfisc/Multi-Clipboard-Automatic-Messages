52ea61b0fb359db621f2e39deaccb1fcf3e528bd
Ethan Fischer <45436178+ejfisc@users.noreply.github.com>
2022-02-07 01:13:13 -0600

    Update mclip.bat

--- mclip.bat
+++ mclip.bat
@@ -1,2 +1,2 @@
-@py.exe C:\<path-name>\mclip.py %*
+@py.exe C:\<path-name>\mclip.py %* # '%*' tells the batch file to listen to commandline arguments (the keyphrase used to select a message)
 @pause
